811919b4e2a02b809966941cf01ada6d54218a6d
Tony Cao <61880125+tcao19@users.noreply.github.com>
2020-03-16 16:47:01 -0400

    Changed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-Tony", # Replace with your own username
+    name="math_package", # Replace with your own username
     version="0.0.1",
     author="Tony Cao",
     author_email="tony.cao@telus.com",
